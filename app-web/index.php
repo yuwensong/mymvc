6a644d982e5942acef01cc6b41f93a987ce196e4
Ailor Yu <yudaxue@163.com>
2016-04-12 03:21:41 +0000

    test hello world

--- app-web/index.php
+++ app-web/index.php
@@ -1,23 +1,2 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE);
-define(ROOT_PATH,dirname(dirname(__FILE__)).'/');
-define(SYS_PATH,'../system');
-define(CUR_PATH,ROOT_PATH.'app-web');
-
-require_once(SYS_PATH.'/functions.php');
-
-$INCLUDE_PATH = array(
-	CUR_PATH,
-	ROOT_PATH.'app-core'
-);
-$CONFIG_PATH = array(
-	CUR_PATH.'/config',
-	ROOT_PATH.'app-core/config',
-	ROOT_PATH.'config',
-);
-
-rsf_require_class('RSF');
-rsf_require_class('WebRequest');
-rsf_require_class('WebResponse');
-
-RSF::get_instance()->run();
+echo "hello world";
